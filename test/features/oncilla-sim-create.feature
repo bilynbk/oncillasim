8a2f4273d124e4dfcff177f3170639db193db3f1
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2012-06-12 19:17:02 +0200

    Added some tests

--- test/features/oncilla-sim-create.feature
+++ test/features/oncilla-sim-create.feature
@@ -3,3 +3,18 @@ Feature: Create a new project
   As an amarsi user
   I would like to create a new project
 
+  Scenario: Create a new project in an empty directory
+    Given I have an empty path
+    When I ask to create a new oncilla project
+    Then I should have a ready to use project in that path
+
+  Scenario: Create a new project in an non-empty directory
+    Given I have a non-empty path
+    When I ask to create a new oncilla project
+    Then I should get an error
+
+  Scenario: Create a new project in an already existing project
+    Given I have an existing project path
+    When I ask to create a new oncilla porject
+    Then I should get an error
+
---
+++ test/features/oncilla_sim_create_steps.py
@@ -0,0 +1,23 @@
+from lettuce import step
+
+@step(u'Given I have an empty path')
+def given_i_have_an_empty_path(step):
+    assert False, 'This step must be implemented'
+@step(u'When I ask to create a new oncilla project')
+def when_i_ask_to_create_a_new_oncilla_project(step):
+    assert False, 'This step must be implemented'
+@step(u'Then I should have a ready to use project in that path')
+def then_i_should_have_a_ready_to_use_project_in_that_path(step):
+    assert False, 'This step must be implemented'
+@step(u'Given I have a non-empty path')
+def given_i_have_a_non_empty_path(step):
+    assert False, 'This step must be implemented'
+@step(u'Then I should get an error')
+def then_i_should_get_an_error(step):
+    assert False, 'This step must be implemented'
+@step(u'Given I have an existing project path')
+def given_i_have_an_existing_project_path(step):
+    assert False, 'This step must be implemented'
+@step(u'When I ask to create a new oncilla porject')
+def when_i_ask_to_create_a_new_oncilla_porject(step):
+    assert False, 'This step must be implemented'
