9917290e96b1f5faaa021a013991c0ae3163e0f2
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2012-06-12 16:35:15 +0200

    Added Authors License

---
+++ README.mk
@@ -0,0 +1,12 @@
+== Oncilla-sim
+
+Oncilla-sim is a collection of tools to help user to install Oncilla simulator.
+
+== How to develop
+
+Please use pip and virtualenv :
+
+    mkvirtualenv oncilla-sim --no-site-packages 
+    workon oncilla-sim
+    pip install -r requirements.txt
+
