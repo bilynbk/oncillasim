6b84dcdb2097f6ed1775887da6324b5a2f6a19c7
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2012-06-12 17:07:24 +0200

    Added basic skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name='oncilla-sim-project-wizard',
+    version='0.1dev',
+    packages=['oncilla-sim'],
+    license='Lesser General Public License version 3',
+    long_description=open('README.mk').read()
+)
+
