bd0f104c16ef4b3ae97b3102a6767a7fe9be8e76
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2012-07-12 14:39:07 +0200

    Released version 0.1.0

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ except OSError, e:
 
 setup(
     name='oncilla-sim-project-wizard',
-    version='0.1.0rc5',
+    version='0.1.0',
     packages=['oncilla_sim'],
     license='Lesser General Public License version 3',
     long_description=open('README.rst').read(),
