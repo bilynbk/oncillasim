36a8e88ff73879032831f0c29d482777241ebff9
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2012-07-12 14:39:51 +0200

    Bumped to versio  0.1.1dev

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ except OSError, e:
 
 setup(
     name='oncilla-sim-project-wizard',
-    version='0.1.0',
+    version='0.1.1dev',
     packages=['oncilla_sim'],
     license='Lesser General Public License version 3',
     long_description=open('README.rst').read(),
