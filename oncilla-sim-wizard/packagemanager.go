1b9472db682f750e96888ab83d3ef0e24b66f563
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2013-04-17 14:25:57 +0100

    Adds SystemDependenciesAreMet

--- oncilla-sim-wizard/packagemanager.go
+++ oncilla-sim-wizard/packagemanager.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type RepositoryDefinition map[string]string
 
 type PackageManager interface {
@@ -13,6 +17,37 @@ type SystemDependencies struct {
 	manager  PackageManager
 	packages []string
 	repDefs  []RepositoryDefinition
+}
+
+func (s *SystemDependencies) SystemDependenciesAreMet() (bool, error) {
+
+	for _, r := range s.repDefs {
+
+		ok, err := s.manager.DoesListRepository(r)
+		if err != nil {
+			return false, err
+		}
+
+		if ok == false {
+			return false, fmt.Errorf("Repository %s is not listed", r)
+		}
+
+	}
+
+	for _, p := range s.packages {
+
+		ok, err := s.manager.HasPackage(p)
+		if err != nil {
+			return false, err
+		}
+
+		if ok == false {
+			return false, fmt.Errorf("Package %s is not installed", p)
+		}
+
+	}
+
+	return true, nil
 }
 
 func (s *SystemDependencies) EnsureSystemDependencies() error {
