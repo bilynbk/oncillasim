12d429a284293a4560154315b61038c7d006a7a3
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2013-04-18 15:43:07 +0100

    Now config is getted dynamically

--- oncilla-sim-wizard/config.go
+++ oncilla-sim-wizard/config.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"regexp"
+)
+
 type GitRepository map[string]string
 
 type PackageList []string
@@ -16,10 +25,48 @@ type Config struct {
 
 var config *Config = nil
 
+func initConfigFromJSON(c *Config) error {
+	var r io.Reader
+	if ok, _ := regexp.MatchString(`\Ahttp://`, options.ConfigPath); ok == true {
+
+		resp, err := http.Get(options.ConfigPath)
+		if err != nil {
+			return err
+		}
+
+		r = resp.Body
+		defer resp.Body.Close()
+
+	} else {
+
+		r, err := os.Open(options.ConfigPath)
+		if err != nil {
+			return err
+		}
+
+		defer r.Close()
+	}
+
+	d := json.NewDecoder(r)
+
+	if err := d.Decode(c); err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
 // Gets a lazily initalized config object
 func GetConfig() *Config {
 	if config == nil {
+
 		config = &Config{}
+		err := initConfigFromJSON(config)
+		if err != nil && options.Verbose == true {
+			fmt.Fprintf(os.Stderr, "Warning during config reading : %s\n", err)
+		}
+
 	}
 
 	return config
